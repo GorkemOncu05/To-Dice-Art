784a4968334d52f8cc50304223a00018baac9ac2
GorkemOncu05 <gorkemoncu05@gmail.com>
2024-07-26 17:37:20 +0300

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,59 @@
+from random import randint
+ques = input('Do you want to dice Y/N : ')
+while ques.upper() == 'Y':
+    print(f'your number is {randint(1,6)}')
+    ques = input('Do you want to roll again !!')
+else:
+    print('Thank you For Playing')
+
+
+
+#Extra dice art use put your project
+
+DICE_ART = {
+    1: (
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘",
+    ),
+    2: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    3: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    4: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    5: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    6: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+}
+DIE_HEIGHT = len(DICE_ART[1])
+DIE_WIDTH = len(DICE_ART[1][0])
+DIE_FACE_SEPARATOR = " "
